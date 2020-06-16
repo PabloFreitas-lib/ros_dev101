9506aab70ce31e2ec63fde4345c043a24de6d9d2
Pablo Freitas <pablo.santos1999@gmail.com>
2020-06-15 23:24:38 -0300

    missing files

---
+++ src/my_minimal_nodes/src/sleepy_minimal_publisher.cpp
@@ -0,0 +1,24 @@
+#include <ros/ros.h>
+#include <std_msgs/Float64.h>
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "sleepy_minimal_publisher"); // Name of the node
+	ros::NodeHandle n;
+	ros::Publisher my_publisher_object = n.advertise<std_msgs::Float64>("topic1", 1);
+
+	std_msgs::Float64 input_float;
+
+	ros::Rate naptime(1.0);
+
+	input_float.data = 0.0;
+
+	while (ros::ok())
+	{
+		input_float.data = input_float.data + 0.001;
+
+		my_publisher_object.publish(input_float);
+
+		naptime.sleep();
+	}
+}
